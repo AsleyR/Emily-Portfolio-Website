b91f716d18cc4ae56c4a3434793ebe6f4d6a0112
AsleyR <asleyrobleto@gmail.com>
2026-01-28 19:31:06 -0500

    Update css styles

--- app/(components)/navbar/Navbar.tsx
+++ app/(components)/navbar/Navbar.tsx
@@ -1,12 +1,18 @@
+import { NavbarMenuProvider } from "@/app/(context)/NavbarMenuContext";
 import Logo from "./Logo";
+import MobileNavbar from "./mobile-navbar/MobileNavbar";
 import NavbarLinks from "./NavbarLinks";
 
 const Navbar = () => {
     return (
-        <nav className="grid grid-cols-2 bg-[#FFFFFF] py-3 px-20 shadow-sm scroll-smooth sticky top-0 z-40">
-            <Logo />
-            <NavbarLinks />
-        </nav>
+            <nav className="grid grid-cols-2 items-center bg-[#FFFFFF] 
+            py-3 px-5 md:px-20 shadow-sm scroll-smooth sticky top-0 z-40 text-main">
+                <Logo />
+                <div className="">
+                    <NavbarLinks />
+                    <MobileNavbar />
+                </div>
+            </nav>
     );
 }
  
