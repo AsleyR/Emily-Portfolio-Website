957ad47f56c07a5d4cd120ff603711be3a5cc322
AsleyR <asleyrobleto@gmail.com>
2026-01-28 19:27:57 -0500

    Added "made by Asley Robleto" text and reformated a little bit the styles

--- app/(components)/ContactMe.tsx
+++ app/(components)/ContactMe.tsx
@@ -17,18 +17,19 @@ const ContactMe = () => {
     ]
 
     return (
-        <section id="contact-me" className="bg-[#d4e4ff] px-20 py-20 grid grid-cols-auto justify-center">
-            <div className="justify-center bg-white p-15 space-y-5">
+        <section id="contact-me" className="bg-[#d4e4ff] px-5 md:px-20 py-20 grid grid-cols-auto 
+        transition-all justify-center">
+            <div className="justify-center bg-white p-5 md:p-15 space-y-5">
                 <div className="flex flex-col space-y-5 col-span-2 place-content-center text-center">
                     <div className="flex flex-col">
-                        <h2 className="font-bold text-5xl tracking-wide">
+                        <h2 className="font-bold text-4xl md:text-5xl tracking-wide">
                             Let's Connect!
                         </h2>
                         <div className="w-full h-auto flex justify-center">
-                            <div className="bg-[#010550] w-[385px] h-[2px]"></div>
+                            <div className="bg-[#010550] w-80 md:w-96.25 h-0.5"></div>
                         </div>
                     </div>
-                    <div className="text-xl leading-widest">
+                    <div className="text-lg md:text-xl leading-widest">
                         <p>
                             I'm excited about the prospect of collaborating on innovative projects that marry finance with creativity. Feel free to reach out to discuss potential opportunities or simply to connect!
                         </p>
