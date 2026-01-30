45350ae4778523287d3a826356621fdd0c3dcc92
AsleyR <asleyrobleto@gmail.com>
2026-01-30 13:30:10 -0500

    Add basic Portfolio page

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,13 @@
+import Portfolio from "../(components)/portfolio/Portfolio";
+
+const PortfolioHomeSection = () => {
+    return (
+        <section className="">
+            <Portfolio />
+            <div className="bg-[#FCA311] w-full h-20"></div>
+            <div className="bg-[#14213D] w-full h-20"></div>
+        </section>
+    );
+}
+ 
+export default PortfolioHomeSection;
