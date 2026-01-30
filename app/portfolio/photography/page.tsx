a4a235c2054310c7c53ad13912eb40dde0c69d0b
AsleyR <asleyrobleto@gmail.com>
2026-01-30 13:30:44 -0500

    Add basic photography page

---
+++ app/portfolio/photography/page.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "@/app/(components)/SectionTitle";
+
+const PhotographyPage = () => {
+    return (
+        <section className="">
+            <SectionTitle title="Photography"/>
+        </section>
+    );
+}
+ 
+export default PhotographyPage;
