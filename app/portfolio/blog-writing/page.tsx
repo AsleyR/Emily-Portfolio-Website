7464cc017c3af4e02afc4ce4705fb10131b8d5c8
AsleyR <asleyrobleto@gmail.com>
2026-01-30 13:41:09 -0500

    Add basic Blog Writing page

---
+++ app/portfolio/blog-writing/page.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "@/app/(components)/SectionTitle";
+
+const BlogWritingPage = () => {
+    return (
+        <section className="">
+            <SectionTitle title="Blog Writing"/>
+        </section>
+    );
+}
+ 
+export default BlogWritingPage;
