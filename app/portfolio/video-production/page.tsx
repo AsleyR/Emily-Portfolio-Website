7dae594f3e2371ca4f4d93768bae5308d066ee8b
AsleyR <asleyrobleto@gmail.com>
2026-01-30 13:30:31 -0500

    Add basic Video Production page

---
+++ app/portfolio/video-production/page.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "@/app/(components)/SectionTitle";
+
+const VideoProductionPage = () => {
+    return (
+        <section className="">
+            <SectionTitle title="Video Production"/>
+        </section>
+    );
+}
+ 
+export default VideoProductionPage;
