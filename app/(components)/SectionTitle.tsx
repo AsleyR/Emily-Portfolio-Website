480adab4e8901a854bac1a3ba5457cf58da13023
AsleyR <asleyrobleto@gmail.com>
2026-02-06 16:32:32 -0500

    Added typewritter effect

--- app/(components)/SectionTitle.tsx
+++ app/(components)/SectionTitle.tsx
@@ -1,6 +1,6 @@
-const SectionTitle = ({ title }: { title: string }) => {
+const SectionTitle = ({ title, className }: { title: string, className?: string }) => {
     return (
-        <h1 className="font-bold text-5xl text-center">
+        <h1 className={`${className} font-bold text-5xl typewriter h-13`}>
             {title}
         </h1>
     );
