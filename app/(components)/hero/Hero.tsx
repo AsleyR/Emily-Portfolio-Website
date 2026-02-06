8616825bd03f9f2e53aa659269a449a4725f8d66
AsleyR <asleyrobleto@gmail.com>
2026-02-06 16:33:55 -0500

    Update Main page hero style

--- app/(components)/hero/Hero.tsx
+++ app/(components)/hero/Hero.tsx
@@ -3,34 +3,26 @@ import ButtonLink from "../BtnLink";
 
 const Hero = () => {
     return (
-        <div className="grid grid-cols-1 space-x-0 hero_bg relative">
-            <div className="text-lg md:text-2xl text-white flex-col px-5 md:px-20 py-20 space-y-8 font-medium z-20 absolute">
-                <h1 className="font-bold text-4xl md:text-6xl">
-                    Nice to meet you!
+        <div className="grid grid-cols-1 space-x-0 hero-bg-pic relative">
+            <div className="text-lg md:text-2xl text-white flex-col px-5 md:px-20 pt-40 
+            pb-20 space-y-8 font-medium z-20 absolute">
+                <h1 className="font-bold text-4xl md:text-7xl">
+                    Hi, I'm Emily.
                 </h1>
                 <div className="space-y-4">
-                    <p className="text-2xl md:text-4xl">
-                    I'm Emily
-                </p>
-                <p className="">
-                    BCom Student at McGill University
-                </p>
+                    {/* <p className="text-2xl md:text-4xl">
+                        I'm Emily
+                    </p> */}
+                    <p className="">
+                        BCom Student at McGill University.
+                    </p>
                 </div>
                 <ButtonLink button={{
                     "link": "/#portfolio",
                     "text": "See my Projects"
                     }}/>
             </div>
-            <div className="w-full h-130 overflow-hidden">
-                <Image
-                className="w-full h-full object-cover"
-                // src={"/media/hero/person-placeholder.jpg"}
-                src={"/media/hero/emily-2.jpg"}
-                height={3000}
-                width={3000}
-                alt="Emily Lim Picture"
-                />
-            </div>
+            <div className="w-full h-150 overflow-hidden"></div>
             <div className="absolute bg-black/40 h-full w-full z-10"></div>
         </div>
     );
